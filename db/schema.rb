e3767493edb7320c140dd26510c3d89e8f631169
Frank <fmendez133@gmail.com>
2013-07-31 20:16:14 -0400

    Fixed sign out on nav bar, added find to user controller

--- db/schema.rb
+++ db/schema.rb
@@ -9,47 +9,47 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130730020318) do
+ActiveRecord::Schema.define(version: 20130730020318) do
 
-  create_table "cookies", :force => true do |t|
+  create_table "cookies", force: true do |t|
     t.string   "name"
     t.string   "color"
     t.integer  "cupcake_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.text     "image"
   end
 
-  create_table "cupcakes", :force => true do |t|
+  create_table "cupcakes", force: true do |t|
     t.string   "name"
     t.integer  "current_score"
     t.integer  "user_id"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.text     "image"
   end
 
-  create_table "frostings", :force => true do |t|
+  create_table "frostings", force: true do |t|
     t.string   "name"
     t.string   "color"
     t.integer  "cupcake_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.text     "image"
   end
 
-  create_table "ice_creams", :force => true do |t|
+  create_table "ice_creams", force: true do |t|
     t.string   "name"
     t.string   "color"
     t.integer  "cupcake_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.text     "image"
   end
 
-  create_table "identities", :force => true do |t|
+  create_table "identities", force: true do |t|
     t.string   "provider"
     t.string   "secret"
     t.string   "uid"
@@ -60,42 +60,42 @@ ActiveRecord::Schema.define(:version => 20130730020318) do
     t.string   "image"
     t.string   "url"
     t.string   "token"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "toppings", :force => true do |t|
+  create_table "toppings", force: true do |t|
     t.string   "name"
     t.string   "color"
     t.integer  "cupcake_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.text     "image"
   end
 
-  create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "name"
     t.boolean  "admin"
-    t.integer  "total_score",            :default => 0
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.integer  "total_score",            default: 0
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "confirmation_token"
     t.string   "unconfirmed_email"
     t.datetime "confirmation_sent_at"
     t.datetime "confirmed_at"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
