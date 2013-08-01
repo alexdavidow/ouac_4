863df3a7d66b1365f04e876e0e147c74efee897e
Frank <fmendez133@gmail.com>
2013-08-01 02:27:03 -0400

    Fixed haml issue with user show page. Added empty divs to user show page. Modified route file to reflect destroy user method and path.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
+  before_filter :user_signed_in?
   def show
-    user = User.find(params[:id])
-    render :show
+    render :show 
+  end
+
+  def destroy
+    User.find(params[:id]).delete
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,14 @@ Ouac4::Application.routes.draw do
   root :to => "home#index"
 
   devise_for :users, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks"}
-  resources :users, only: [:show]
 
+  resources :users, only: [:show, :destroy]
   resources :cupcakes
   resources :cookies, :only => [:index, :new, :create, :destroy]
   resources :frostings, :only => [:index, :new, :create, :destroy]
   resources :toppings, :only => [:index, :new, :create, :destroy]
   resources :ice_creams, :only => [:index, :new, :create, :destroy]
+  delete 'users/delete/.:id' => 'users#destroy', as: 'delete_user'
   get 'win' => 'cupcakes#win'
   get 'lose' => 'cupcakes#lose'
   get 'sessions/new' => 'sessions#new'
