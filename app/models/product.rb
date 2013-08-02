4f7e428b721fdd528ebc89c818efb6304c5a7026
Frank <fmendez133@gmail.com>
2013-08-02 15:23:53 -0400

    Added product orders controller. Added dependent destroy to carts so if a user is destroyed, a cart is destroyed as well.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,17 @@ class Product < ActiveRecord::Base
   attr_accessible :name, :price, :quantity
   has_many :product_orders
   has_one :shopping_carts, through: :product_orders
+
+  before_destroy ensure_not_referenced_by_order
+
+  private
+
+  def ensure_not_referenced_by_order
+    if product_orders.empty?
+      return true
+    else
+      errors.add(:base, 'Products are present in orders')
+      return false
+    end
+  end
 end
