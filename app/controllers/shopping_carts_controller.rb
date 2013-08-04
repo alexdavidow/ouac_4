e85782bc6a714e3d27ffc3eb125b9d2247bf263f
Frank <fmendez133@gmail.com>
2013-08-04 04:45:29 -0400

    Created add item logic in cart controller. Removed twitter auth link from homepage. Changed the id of each item on the product index page to data-id for jquery reasons. Created js partial for situational loading of js files.

--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -3,5 +3,20 @@ class ShoppingCartsController < ApplicationController
   before_filter :user_signed_in?
   def show
     @items = current_cart.product_orders.all
+    @cart = current_cart
+  end
+
+  def add_item
+    if current_cart
+      cart = current_cart
+      item = ProductOrder.find(params[:id])
+      cart.product_orders << item
+      respond_to do |format|
+        format.html { redirect_to shopping_cart_path, notice: "You have successfully added #{item.name} to your cart." }
+        #format.json ------- ### Add for AJAX call 
+      end
+    else
+      flash[:error] = "There was a problem adding the item to your cart. Please refresh and try again."
+    end
   end
 end
