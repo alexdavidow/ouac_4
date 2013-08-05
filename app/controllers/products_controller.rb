68617307771fb337cc035198cd0a20a82c1104d8
alexdavidow <alex.davidow@gmail.com>
2013-08-04 22:32:45 -0400

    working on ajax call but having problem with id in url

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,9 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def show
+    @product = Product.find(params[:id])
+    render :json => @product.to_json
+  end
 end
