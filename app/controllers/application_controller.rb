7468d499bdf6acd059921a57e0c3220c22ab7e13
alexdavidow <alex.davidow@gmail.com>
2013-08-07 00:54:59 -0400

    users updating new attributes finally

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  protect_from_forgery with: :exception
 
   def stored_location_for(resource)
     nil
@@ -7,5 +7,11 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     root_path
+  end
+
+  before_filter :update_sanitized_params, if: :devise_controller?
+
+  def update_sanitized_params
+    devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:email, :shopping_cart_id, :password, :password_confirmation, :remember_me, :identities, :admin, :name, :total_score, :phone, :address, :current_password )}
   end
 end
