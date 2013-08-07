c5fb823237727f63a326e3aa7990020503e12f0e
Frank <fmendez133@gmail.com>
2013-08-07 15:38:18 -0400

    Moved current_cart method to a helper and extended it within application helper. Created table row in view for updete cart method.

--- app/helpers/shopping_cart_helper.rb
+++ app/helpers/shopping_cart_helper.rb
@@ -1,11 +1,11 @@
 module ShoppingCartHelper
   def current_cart
-    # @cart = current_user.shopping_cart # Relies on current_user helper. Bad practice to create a dependecy on another helper.
-      user = current_user
-      if user.shopping_cart
-        @cart = user.shopping_cart
-      else
-        @cart = ShoppingCart.new(user: user, user_id: user.id)
-      end
+    user = current_user
+    if user.shopping_cart
+      @cart = user.shopping_cart
+    else
+      @cart = ShoppingCart.new(user: user, user_id: user.id)
+    end
+    @cart
   end
 end
