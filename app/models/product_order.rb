73f25ee07386b94d9c49a16d9e4748debd437f0d
alexdavidow <alex.davidow@gmail.com>
2013-08-07 12:50:35 -0400

    pulling

--- app/models/product_order.rb
+++ app/models/product_order.rb
@@ -2,11 +2,5 @@ class ProductOrder < ActiveRecord::Base
   attr_accessible :order_id, :product_id, :product, :quantity, :price
   belongs_to :order, dependent: :destroy
   belongs_to :product
-
-  # Only want to validate current_users attrs once they hit product_orders/new
-  # validates_presence_of :"current_user.name"
-  # validates_presence_of :"current_user.email"
-  # validates_presence_of :"current_user.phone"
-  # validates_presence_of :"current_user.address"
   
 end
