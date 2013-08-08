ccfa9c43a54b99a9ee5726f5ec190ec269d41d49
Frank <fmendez133@gmail.com>
2013-08-08 01:56:33 -0400

    Add logic to product orders controller to empty cart after successful payment is made.

--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -5,6 +5,20 @@ class ShoppingCart < ActiveRecord::Base
 
   after_create :create_order
 
+
+  def empty_cart
+    puts "BEGIN CART DUMP"
+    # Find the current order within the cart
+    current_order = self.order
+    # Remove the association between the cart and the order, freeing the cart itself temporarily
+    current_order.shopping_cart_id = nil
+    puts "DUMPED ORDER RELATIONSHIP"
+    self.order = nil
+    puts "DUMPED CART RELATIONSHIP"
+    # Create new order via create_order
+    self.create_order
+  end
+
   def create_order
     self.order = Order.create(shopping_cart_id: self.id, shopping_cart: self)
   end
