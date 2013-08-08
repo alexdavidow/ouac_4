388468ed1dabc84c5d30be3dd533850e0fa45b07
Frank <fmendez133@gmail.com>
2013-08-08 01:05:00 -0400

    Update add to cart add_without_duplicates method to correctly save values to cart without creating multiple product orders.

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -1,13 +1,4 @@
 module CurrentCart
   extend ActiveSupport::Concern
 
-  def current_cart
-    # @cart = current_user.shopping_cart # Relies on current_user helper. Bad practice to create a dependecy on another helper.
-      user = current_user
-      if user.shopping_cart
-        @cart = user.shopping_cart
-      else
-        @cart = ShoppingCart.new(user: user, user_id: user.id)
-      end
-  end
 end
